91f4e5d7efeaf4503793fbbda75b6a95491b6ee2
Leon Dail <leondailofficial@gmail.com>
2019-10-29 12:46:55 -0700

    added protected endpoints and login functionality

---
+++ test/reviews-endpoints.spec.js
@@ -0,0 +1,143 @@
+const knex = require('knex')
+const app = require('../src/app')
+const helpers = require('./test-helpers')
+
+describe('Reviews Endpoints', function() {
+  let db
+
+  const {
+    testThings,
+    testUsers,
+  } = helpers.makeThingsFixtures()
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
+
+  after('disconnect from db', () => db.destroy())
+
+  before('cleanup', () => helpers.cleanTables(db))
+
+  afterEach('cleanup', () => helpers.cleanTables(db))
+
+  describe(`POST /api/reviews`, () => {
+    beforeEach('insert things', () =>
+      helpers.seedThingsTables(
+        db,
+        testUsers,
+        testThings,
+      )
+    )
+
+    it(`If there is no authorization present, then give a 401 status error`,() => {
+      return supertest(app)
+        .post('/api/reviews')
+        .expect(401, { error: `Missing basic token`});
+    })
+
+    it(`If there are no credentials in token, then give 401 error`,() => {
+      const noUserCreds = { user_name: '', password: ''};
+
+
+      return supertest(app)
+        .post('/api/reviews')
+        .set('Authorization', helpers.makeAuthHeader(noUserCreds))
+        .expect(401, { error: `Unauthorized request`});
+    });
+
+    it (`If there is an invalid username, respond with 401`, () => {
+      const userInvalidCreds = { user_name: 'notAUser', password: 'NotAPassword4Sure' };
+
+
+      return supertest(app)
+        .post('/api/reviews')
+        .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
+        .expect(401, { error: `Unauthorized request`});
+    })
+
+    it (`If there is an invalid password, respond with 401`, () => {
+      const userInvalidCreds = { user_name: testUsers[0], password: 'NotAPassword4Sure' };
+
+
+
+      return supertest(app)
+        .post('/api/reviews')
+        .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
+        .expect(401, { error: `Unauthorized request`});
+    })
+
+    it(`creates an review, responding with 201 and the new review`, function() {
+      this.retries(3)
+      const testThing = testThings[0]
+      const testUser = testUsers[0]
+      const newReview = {
+        text: 'Test new review',
+        rating: 3,
+        thing_id: testThing.id,
+      }
+
+      return supertest(app)
+        .post('/api/reviews')
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .send(newReview)
+        .expect(201)
+        .expect(res => {
+          expect(res.body).to.have.property('id')
+          expect(res.body.rating).to.eql(newReview.rating)
+          expect(res.body.text).to.eql(newReview.text)
+          expect(res.body.thing_id).to.eql(newReview.thing_id)
+          expect(res.body.user.id).to.eql(testUser.id)
+          expect(res.body.user_id).to.eql(testUser.id)
+          expect(res.headers.location).to.eql(`/api/reviews/${res.body.id}`)
+          const expectedDate = new Date().toLocaleString()
+          const actualDate = new Date(res.body.date_created).toLocaleString()
+          expect(actualDate).to.eql(expectedDate)
+        })
+        .expect(res =>
+          db
+            .from('thingful_reviews')
+            .select('*')
+            .where({ id: res.body.id })
+            .first()
+            .then(row => {
+              expect(row.text).to.eql(newReview.text)
+              expect(row.rating).to.eql(newReview.rating)
+              expect(row.thing_id).to.eql(newReview.thing_id)
+              expect(row.user_id).to.eql(newReview.user_id)
+              const expectedDate = new Date().toLocaleString()
+              const actualDate = new Date(row.date_created).toLocaleString()
+              expect(actualDate).to.eql(expectedDate)
+            })
+        )
+    })
+
+    const requiredFields = ['text', 'rating', 'thing_id']
+
+    requiredFields.forEach(field => {
+      const testThing = testThings[0]
+      const testUser = testUsers[0]
+      const newReview = {
+        text: 'Test new review',
+        rating: 3,
+        thing_id: testThing.id,
+      }
+
+
+      it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+        delete newReview[field]
+
+        return supertest(app)
+          .post('/api/reviews')
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .send(newReview)
+          .expect(400, {
+            error: `Missing '${field}' in request body`,
+          })
+      })
+    })
+  })
+})
